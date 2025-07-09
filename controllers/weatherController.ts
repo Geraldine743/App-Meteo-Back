25adf063cbfc2b2c4c6aba9a5bf9bb49f13393a0
Geraldine743 <egeraldine2@gmail.com>
2025-07-09 09:39:10 +0200

    update callAPI and add MinimalWeatherData interface

--- controllers/weatherController.ts
+++ controllers/weatherController.ts
@@ -3,6 +3,7 @@ import axios, {AxiosResponse} from "axios";
 import { WEATHER_API_ERROR_MESSAGE } from "../constantes/errorMessage";
 import { WEATHER_API_URL } from "../constantes/config";
 import { ApiError } from "../errors/ApiError";
+import { MinimalWeatherData } from "../interfaces/MinimalWeatherData";
 
 export class WeatherController {
     private API_KEY : string;
@@ -15,10 +16,16 @@ export class WeatherController {
         const city : string = req.params.city;
         try{
             const response : AxiosResponse = await axios.get(
-                `${WEATHER_API_URL}current.json?key=${this.API_KEY}&q=${city}`
+                `${WEATHER_API_URL}current.json?key=${this.API_KEY}&q=${city}&lang=fr`
             );
-            const data = response.data;
-            res.json(data);
+            const minimalData: MinimalWeatherData = {
+                city:response.data.location.name,
+                country:response.data.location.country,
+                temperature:response.data.current.temp_c,
+                condition:response.data.current.condition.text,
+                icon:response.data.current.condition.icon
+            }
+            res.json(minimalData);
         }catch(error){
             next(new ApiError(WEATHER_API_ERROR_MESSAGE));
         }
